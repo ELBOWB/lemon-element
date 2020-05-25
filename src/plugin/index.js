dabf810c3653511a75091035569b2f37b88059ce
wenbingyan <wenbingyan@kanzhun.com>
2020-05-25 11:13:40 +0800

    fixed: remove console

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -2,7 +2,6 @@ import * as components from './components'
 var VuePlugin = {}
 VuePlugin.install = function(Vue, options) {
     for(let component in components) {
-        console.log(component,'component')
         Vue.component('le'+component, components[component]);
     }
 }
